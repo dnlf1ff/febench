3865ecf98b1ae6610e96b5fedb58ffb111676961
bffntocffn <jinvk.to@gmail.com>
2025-09-17 15:24:47 +0900

    commensurate

--- examples/aug.py
+++ examples/aug.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import sys
 head = '/data2_1/jinvk/25_Fe'
 
 model_dct = {
@@ -14,7 +14,7 @@ model_dct = {
     'nequip': {'modal': ['oam'], 'label': 'NequIP'},
         }
 
-run_params = {'data': '250916', 'fmax': ['1e-2', '0.5e-3'], 'cell': ['ucf', 'frech']}
+run_params = {'date': '250916', 'fmax': ['1e-2', '0.5e-3'], 'cell': ['ucf', 'frech']}
 carbon_dft = [0.47, -0.01, 0.68, 0.47, 0.34, 0.58, 0.12, 0.16, 0.13, -0.09, -0.09, -0.65, -1.67, 1.07, 1.5] 
 tm_dft = [-0.043, -0.239, 0.246, 0.079, -0.277, -0.381, 0.02, -0.246, -0.233]
 carbon_config = ['a1', 'a2', 'b1', 'b2', 'b3', 'c1', 'c2', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
@@ -22,35 +22,76 @@ tm_config = ['Co', 'Cr', 'Cu', 'Mn', 'Mo', 'Nb', 'Ni', 'Ti', 'V']
 import sys
 
 def save_carbon(fmax, cell):
-    prefix = f'{run_params["data"]}_fmax_{fmax}_{cell}'
+    prefix = f'{run_params["date"]}_fmax_{fmax}_{cell}'
     df = pd.DataFrame(index=carbon_config)
     df['dft'] = carbon_dft
     for model, dct in model_dct.items():
         for modal in dct['modal']:
             try:
                 df_carbon = pd.read_csv(f'{head}/{prefix}/{model}/{modal}/carbon/carbon.csv', comment='#')
-                df[f'{model}-{modal}']  = df_carbon['E_bind']
+                df[f'{model}-{modal}']  = df_carbon['config'].tolist() #TODO
             except:
                 print(f'ERROR:{model}-{modal} {fmax} {cell}')
     df.to_csv(f'carbon_aug_fmax_{fmax}_{cell}.csv')
 
 def save_tm(fmax, cell):
-    prefix = f'{run_params["data"]}_fmax_{fmax}_{cell}'
+    prefix = f'{run_params["date"]}_fmax_{fmax}_{cell}'
     df = pd.DataFrame(index=tm_config)
     df['dft'] = tm_dft
     for model, dct in model_dct.items():
         for modal in dct['modal']:
             try:
-                df_tm = pd.read_csv(f'{head}/{prefix}/{model}/{modal}/tm/tm_E_bind.csv', comment='#')
-                df[f'{model}-{modal}']  = df_tm['E_bind']
+                df_tm = pd.read_csv(f'{head}/{prefix}/{model}/{modal}/tm/tm_E_bind.csv', ) # comment=#
+                df[f'{model}-{modal}'] = df_tm['E_Fe'].tolist() # need to fix this
             except Exception as exc:
                 print(f'ERROR:{model}-{modal} {fmax} {cell}')
-                print(exc)
     df.to_csv(f'tm_aug_fmax_{fmax}_{cell}.csv')
+
+
+def get_carbon_errors(fmax, cell):
+    suffix = f'aug_fmax_{fmax}_{cell}'
+    carbon_df = pd.read_csv(f'carbon_{suffix}.csv', index_col=0)
+    carbon_true = carbon_df['dft']
+    print(carbon_true)
+    carbon_error = pd.DataFrame(index=carbon_config)
+    for col in carbon_df.columns:
+        print(carbon_df[col])
+        carbon_error[col] = carbon_df[col] - carbon_true
+
+    carbon_error.to_csv(f'carbon_error_fmax_{fmax}_{cell}.csv')
+    
+def get_tm_errors(fmax, cell):
+    suffix = f'aug_fmax_{fmax}_{cell}'
+    tm_df = pd.read_csv(f'tm_{suffix}.csv', index_col=0)
+    tm_true = tm_df['dft']
+    print(tm_true)
+    tm_error = pd.DataFrame(index=tm_config, index_col=0)
+    for col in tm_df.columns:
+        print(tm_df[col])
+        tm_error[col] = tm_df[col] - tm_true
+
+    tm_error.to_csv(f'tm_error_fmax_{fmax}_{cell}.csv')
+ 
+
+def get_abs(fmax, cell):
+    carbon_error = pd.read_csv(f'carbon_error_fmax_{fmax}_{cell}.csv', index_col=0)
+    for col in carbon_error.columns:
+        print(carbon_error[col])
+        carbon_error[col] = carbon_error[col].apply(lambda x: abs(x))
+        print(carbon_error[col])
+    carbon_error.to_csv(f'carbon_abs_fmax_{fmax}_{cell}.csv')
+
+    tm_error = pd.read_csv(f'tm_error_fmax_{fmax}_{cell}.csv', index_col=0)
+    for col in tm_error.columns:
+        print(tm_error[col])
+        tm_error[col] = tm_error[col].apply(lambda x: abs(x))
+        print(tm_error[col])
+    carbon_error.to_csv(f'tm_abs_fmax_{fmax}_{cell}.csv')
+
+
 
 if __name__ == '__main__':
     for fmax in run_params['fmax']:
         for cell in run_params['cell']:
-            save_carbon(fmax=fmax, cell=cell)
-            save_tm(fmax=fmax, cell=cell)
+            get_abs(fmax, cell)
 
